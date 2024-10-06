4bceee19d9939a24cdd603de213d63e47288b3ac
AndreOn <94354513+AndreiKirV@users.noreply.github.com>
2024-10-06 18:32:06 +0300

    NavMesh

--- Assets/Scripts/MBS/MinionMB.cs
+++ Assets/Scripts/MBS/MinionMB.cs
@@ -1,15 +1,17 @@
+using System;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.EventSystems;
-using UnityEngine.InputSystem;
 
-public class MinionMB : MonoBehaviour, IPointerUpHandler
+public class MinionMB : MonoBehaviour
 {
     public NavMeshAgent Agent;
+    private bool _isSelected = false;
+    private InputSettings _input;
 
-    private void Start()
+    private void Awake()
     {
-
+        _input = new InputSettings();
+        _input.Enable();
     }
 
     private void Update()
@@ -17,23 +19,36 @@ public class MinionMB : MonoBehaviour, IPointerUpHandler
 
     }
 
-    private void OnMouseEnter()
-    {
-        Debug.Log("Enter");
-    }
-
     private void OnMouseDown()
     {
         Debug.Log("down");
+        _isSelected = true;
+        _input.Mouse.RB.started += GoMove;
+        _input.Mouse.LB.started += Deselect;
+        _input.Enable();
     }
 
-    private void OnMouseUp()
+    private void GoMove(UnityEngine.InputSystem.InputAction.CallbackContext context)
     {
-        Debug.Log("up");
+        if (!_isSelected) return;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            // Если луч пересекает объект с коллайдером, выводим точку попадания в лог
+            Debug.Log($"Moving to {hit.point}");
+
+            // Для демонстрации — перемещение миньона к точке на NavMesh
+            Agent.SetDestination(hit.point);
+        }
     }
 
-    public void OnPointerUp(PointerEventData eventData)
+    private void Deselect(UnityEngine.InputSystem.InputAction.CallbackContext context)
     {
-        Debug.Log("up");
+        _isSelected = false;
+        _input.Mouse.RB.started -= GoMove;
+        _input.Mouse.LB.started -= Deselect;
     }
 }
